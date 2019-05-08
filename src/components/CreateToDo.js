fb177b3448843fe0b2e06473d48d286e527b5201
Angelo Machado Fonseca <angelofmfonseca@gmail.com>
2019-05-08 00:12:28 -0300

    estrutura do formulário finalizada - iniciar backend

--- src/components/CreateToDo.js
+++ src/components/CreateToDo.js
@@ -47,7 +47,67 @@ class CreateToDo extends Component {
     }
     render(){
         return(
-            <h1>CreateToDo</h1>
+            <div style={{ marginTop: 20 }}>
+                <h3>Create New ToDo</h3>
+                <form onSubmit={ this.onSubmit }>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text" 
+                            className='form-control'
+                            value={ this.state.todo_description }
+                            onChange={ this.onChangeToDoDescription } />
+                    </div>                    
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input type="text" 
+                            className='form-control'
+                            value={ this.state.todo_responsible }
+                            onChange={ this.onChangeToDoResponsible }
+                        />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input type="radio" 
+                                name='priorityOptions'
+                                id='priorityLow'
+                                value='Low'
+                                checked={ this.state.todo_priority === 'Low' }
+                                className="form-check-input"
+                                onChange={ this.onChangeToDoPriority } 
+                            />
+                            <label className="form-check-label">Low</label>
+                        </div>                        
+                        <div className="form-check form-check-inline">
+                            <input type="radio" 
+                                name='priorityOptions'
+                                id='priorityMedium'
+                                value='Medium'
+                                checked={ this.state.todo_priority === 'Medium' }
+                                className="form-check-input"
+                                onChange={ this.onChangeToDoPriority } 
+                            />
+                            <label className="form-check-label">Medium</label>
+                        </div>                        
+                        <div className="form-check form-check-inline">
+                            <input type="radio" 
+                                name='priorityOptions'
+                                id='priorityHigh'
+                                value='High'
+                                checked={ this.state.todo_priority === 'High' }
+                                className="form-check-input"
+                                onChange={ this.onChangeToDoPriority } 
+                            />
+                            <label className="form-check-label">High</label>
+                        </div>
+                        <div className="form-group" style={{ marginTop: 20 }}>
+                            <input type="submit" 
+                                value="Create ToDo"
+                                className='btn btn-primary'
+                            />
+                        </div>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
