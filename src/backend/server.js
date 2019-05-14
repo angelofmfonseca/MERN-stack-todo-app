4ca67de76fed212665e037128fd909625cfccc07
Angelo Machado Fonseca <angelofmfonseca@gmail.com>
2019-05-14 00:10:12 -0300

    fazendo os endpoints - fazer o update endpoint

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+
 const todoRoutes = express.Router()
 
 const PORT = 4000
@@ -21,6 +22,38 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log(`Connection to database established successfully`)
 })
+
+todoRoutes.route('/').get((req, res) => {
+    todo.find((err, todos) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.json(todos)
+        }
+    })
+})
+
+todoRoutes.route('/:id').get((req, res) => {
+    let id = req.params.id
+    todo.findById(id, (err, todo) => {
+        res.json(todo)
+    })
+})
+
+todoRoutes.route('/add').post((req, res) => {
+    let todo = new Todo(req.body)
+    todo.save()
+        .then((todo) => {
+            res.status(200).json({
+                'todo': 'ToDo added successfully'
+            })
+        .catch((err) => {
+            res.status(400).send('Adding new ToDo failed')
+        })
+        })
+})
+
+app.use('/todos', todoRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${ PORT }`);
