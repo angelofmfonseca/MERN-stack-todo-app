2a8a0e7218957bffe48e5f1672611c539f71f1b0
Angelo Machado Fonseca <angelofmfonseca@gmail.com>
2019-06-03 01:09:56 -0300

    isolando o ToDo Component

--- src/frontend/ToDoList.js
+++ src/frontend/ToDoList.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-const ToDo = props => (
-    <tr>
-        <td className={ props.todo.todo_completed ? 'completed' : '' }>
-            { props.todo.todo_description }
-        </td>
-        <td className={ props.todo.todo_completed ? 'completed' : '' }>
-            { props.todo.todo_responsible }
-        </td>
-        <td className={ props.todo.todo_completed ? 'completed' : '' }>
-            { props.todo.todo_priority }
-        </td>
-        <td>
-            <Link to={ '/edit/' + props.todo._id }>Edit</Link>
-        </td>
-    </tr>
-)
+import ToDo from './ToDo'
 
 class ToDoList extends Component {
     constructor(props){
         super(props)
         this.state = {
-            todos:[]
+            todos: []
         }
     }
 
